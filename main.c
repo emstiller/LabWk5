ece31caa5d9b8cab8058043ae157ede4c907a683
Emily <emily@LAPTOP-ANMJIU9K.(none)>
2020-03-27 15:48:19 +1100

    LabWk5
    Task 8

--- main.c
+++ main.c
@@ -1,36 +1,24 @@
 #include <stdio.h>
-#include <math.h> // so we can use expf()
 
-//function prototye for integral calculation
+float diff(float x, float h);
 float f(float x);
 
 int main()
 {
-   int N = 50; //number of trapezoids
-   int k; // loop counter
-   float x1 = 0, x2 = 2; //endpoints
-   float dx = (x2 - x1)/N; //calculate widths of pyramids
-   float fx; //integral calculation
-
-   // evaluation of the sum of
-   //k = 1 to N - 1 for f(k)
-   float sum = 0;
-   for(k = 1; k < N; k++)
-   {
-       float x;
-       x = x1 + k*dx; //first calculate the value to go into the function
-       sum = sum + f(x); //calculate the function, add to existing value
-   }
-
-   fx = dx*(0.5*f(x1) + sum + 0.5*f(x2)); //calculate integral
-
-   printf("%f", fx); //print result
-
-   return 0;
+    float h = 0.0001;
+    printf("%f\n", diff(0, h));
+    printf("%f\n", diff(1, h));
+    printf("%f\n", diff(-1, h));
 }
 
+float diff(float x,float h)
+{
+    float d;
+    d = (f(x + h) - f(x))/h;
+    return d;
+}
 //function definition
 float f(float x)
 {
-    return expf(x*x); //e^(x^2), the function to be integrated
+    return x*x + 2*x - 1;
 }
